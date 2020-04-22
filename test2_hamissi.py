da4d260af87c08e7c9a741b97ad98adbc589d4df
Reihan35 <hamissi20@yahoo.com>
2020-04-22 23:50:13 +0200

    debugging on preprocessing

--- test2_hamissi.py
+++ test2_hamissi.py
@@ -191,17 +191,20 @@ def find_pattern(gprim,g,Vprim,mapi):
 
 def preprocessing(E,T):
     pi = []
-    for i in range (1,T+1):
+    for i in range (0,T-1):
         pi.append(-1)
     k = -1
-    for i in range (1,T+1):
+    for i in range (0,T-1):
+        print("k " + str(k))
+        print("i " + str(i))
+        print("E[k+1]" + str(E[k+1]))
+        print("E[i]" + str(E[i]))
         while k >= 0 and graphsequal(E[k+1],E[i]) == False:
             k=pi[k]
         k = k+1
         pi[i] = k
     return pi
 
-#print(preprocessing(to_list_of_matrices(file_to_graphs("/home/fatemeh/Bureau/Stage/pattern.txt")),2,to_list_of_matrices(file_to_graphs("/home/fatemeh/Bureau/Stage/target.txt")),5))
         
 def path_stream_matching(E,T,Eprim,Tprim,Vprim):
     pi = preprocessing(E,T)
@@ -226,5 +229,6 @@ example_target = to_list_of_matrices(file_to_graphs("/home/fatemeh/Bureau/Stage/
 example_pattern = to_list_of_matrices(file_to_graphs("/home/fatemeh/Bureau/Stage/example_pattern.txt"),4)
 
 
+#print(preprocessing(example_pattern,2))
 
 print(path_stream_matching(example_pattern,2,example_target,2,[0,1,2,3]))
